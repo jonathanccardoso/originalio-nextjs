7d1906a3a137168b8f6d633d82302d77a378b09e
jonathanccardoso <jonathan99moura@hotmail.com>
2020-08-11 17:31:46 -0300

    Fixme: adds product pages

---
+++ config/stripe.ts
@@ -0,0 +1,6 @@
+export default {
+  publicKey:
+    "pk_test_51HF29oL0x9kpTTRHGnHI27nhyCxO559nCow8qrHorNGVwB7noe4nU6sPAFRmSGZx12ggPxo9mu6vsNeHNfHjmvPl00P4FZaiRU",
+  secretKey:
+    "sk_test_51HF29oL0x9kpTTRHdehrgHjcaWQQ6LrQJwZvnguHSAEFGdBUr9m89dYAlVHM772gBm5mKYlgyAWs7vILWCzIRME800SIrHmFw9",
+};
--- pages/[productId].tsx
+++ pages/[productId].tsx
@@ -1,7 +1,69 @@
 import React from "react";
+import Stripe from "stripe";
+import { GetStaticPaths, GetStaticProps } from "next";
 
-const Product: React.FC = () => {
-  return <h1>Product</h1>;
+import stripeConfig from "../config/stripe";
+
+interface Props {
+  product: Stripe.Product;
+}
+
+// next: create page
+export const getStaticPaths: GetStaticPaths = async () => {
+  const stripe = new Stripe(stripeConfig.secretKey, {
+    apiVersion: "2020-03-02",
+  });
+
+  // each stored product
+  const products = await stripe.products.list();
+
+  // console.log("products", products.data);
+
+  // one page for each registered product
+  const paths = products.data.map((product) => ({
+    params: {
+      productId: product.id,
+    },
+  }));
+
+  // console.log("paths", paths);
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+// next: get the information from the page
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const stripe = new Stripe(stripeConfig.secretKey, {
+    apiVersion: "2020-03-02",
+  });
+
+  const { productId } = params;
+
+  const product = await stripe.products.retrieve(productId as string);
+
+  console.log("product", product);
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
+
+const Product: React.FC<Props> = ({ product }) => {
+  return (
+    <div>
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      {product.images && (
+        <img src={product.images} style={{ width: "300px" }} />
+      )}
+      <h2>R$ 20,00</h2>
+    </div>
+  );
 };
 
 export default Product;
